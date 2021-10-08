5ad892afb74240305507185e1c73209fc3f6375b
caiohscruz <caiohscruz@hotmail.com>
2021-10-08 17:32:33 -0300

    update join

--- join.js
+++ join.js
@@ -1,7 +1,9 @@
 const database = require("./database")
 
-database.select(["owner.name as owner", "animal.name as animal"]).table("owner").innerJoin("animal","owner.id","animal.owner").then(data=> {
-    console.log(data);
-}).catch(err =>{
+database.select(["owner.name as owner", "animal.name as animal"]).table("owner").innerJoin("animal", "owner.id", "animal.owner").then(data => {
+    data.forEach(element => {
+        console.log(`${element.animal} é animal de ${element.owner}`)
+    });
+}).catch(err => {
     console.log(err);
 })
